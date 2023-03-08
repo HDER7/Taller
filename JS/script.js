082fbf285dd487bbf0ac437681ddb5fafef6def9
Heider Rivas <heiderarb@gmail.com>
2023-03-08 16:47:38 -0500

    Falta mis habilidades y experencia

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,6 @@ function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("nav").style.top = "0";
     } else {
-        document.getElementById("nav").style.top = "-50px";
+        document.getElementById("nav").style.top = "-10px";
     }
 }
